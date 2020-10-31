0bb68cd1ca3dde63e91ff6764929d1259fe54c20
Salamandr72 <Salamandr72@mail.ru>
2020-10-31 21:46:07 +0300

    feat: update files

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (Array.isArray(arr)) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'string') {
+            if (Array.isArray(arr[i])) continue;
+            str = str + arr[i].replace(/\s/g, '').match(/\b^[A-Za-z]/);
+        }
+    }
+    return str.toUpperCase().split('').sort().join('');
+} else return false;
 };
